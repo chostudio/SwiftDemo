8b9630045e81ba61754f9d77fe198b4875415574
Chris H <officialchrisho@gmail.com>
2023-06-19 17:46:08 -1000

    Initial Commit

---
+++ Demo App/Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_AppApp.swift
+//  Demo App
+//
+//  Created by Chris Ho on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

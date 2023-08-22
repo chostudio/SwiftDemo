4853b4e914ab1a0ba09b3f6237e1145966466b8b
Chris H <officialchrisho@gmail.com>
2023-08-21 19:48:04 -1000

    Changed text

--- Demo App/ContentView.swift
+++ Demo App/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+                .multilineTextAlignment(.center)
+                
+                
         }
         .padding()
     }

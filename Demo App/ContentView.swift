6fab0801ef4e9cdd0b72fb1dd68b42bbfea2a790
Chris H <officialchrisho@gmail.com>
2023-08-25 21:03:03 -1000

    Added Content

--- Demo App/ContentView.swift
+++ Demo App/ContentView.swift
@@ -10,15 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .font(.title)
+            Text("Whispr")
+                .font(.largeTitle)
                 .fontWeight(.bold)
-                .foregroundColor(Color.green)
+                .foregroundColor(Color.blue)
                 .multilineTextAlignment(.center)
+            
+            Image(systemName: "mic.circle.fill")
+                .resizable()
+//                .imageScale(.medium)
+                .frame(width: 100,height: 100 )
+                .foregroundColor(Color.blue)
                 
-                
+                //when clicked turn red
+            Text("Click to start recording")
+                .font(.body)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+                .multilineTextAlignment(.center)
+                .padding()
         }
         .padding()
+        //options where to send after recording
+        HStack{
+            Text("Send text via message")
+//            message.fill
+//            square.and.arrow.up
+            Text("Send text via ")
+            
+            
+        }
+        //bottom bar
+        //        info.square
     }
 }
 
